b989e13e5afafcdbc1cc43650f8c838d7189c91f
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2021-11-13 09:33:13 +0300

    initial commit for Kazi Bora

---
+++ app/src/main/java/com/magiri/KaziBora/MainActivity.java
@@ -0,0 +1,35 @@
+package com.magiri.KaziBora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button EmployerBtn,EmployeeBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EmployeeBtn=findViewById(R.id.employeeButton);
+        EmployerBtn=findViewById(R.id.employerButton);
+
+        EmployerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),EmployerLogin.class));
+            }
+        });
+
+        EmployeeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),EmployeeLogin.class));
+            }
+        });
+
+
+    }
+}
